641bdc5ec9e4fdcb430f48d88f1e1476d16f4126
Mohammed Helal <mohd.omar.helal@gmail.com>
2023-08-16 10:42:23 +0800

    finalizing code and styling

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -2,46 +2,64 @@ import { useState } from "react";
 
 function ComingSoon() {
   const [email, setEmail] = useState("");
-  const [isError, setError] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const emailRegexValidation = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-  function handleSubmit() {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    document.getElementById("input").value = "";
+
     if (!email || email === "" || !emailRegexValidation.test(email)) {
-      setError(true);
+      setIsError(true);
       return;
     }
+
+    setIsError(false);
+    console.log(email);
   }
 
   const inputStyle = "email-input " + (isError ? "email-input-error" : "");
 
   return (
     <>
-      <img className="logo" src="./images/logo.svg" alt="Ping logo" />
-      <h1 className="heading">
-        We are launching <span className="heading-soon">soon!</span>
-      </h1>
-      <p className="subscribe">Subscribe and get notified Notify Me</p>
+      <div className="header">
+        <img className="logo" src="./images/logo.svg" alt="Ping logo" />
+        <h1 className="heading">
+          We are launching <span className="heading-soon">soon!</span>
+        </h1>
+        <p className="subscribe">Subscribe and get notified Notify Me</p>
+      </div>
       <form className="email-form" onSubmit={handleSubmit}>
-        <input
-          className={inputStyle}
-          type="email"
-          value={email}
-          placeholder="Your email address..."
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <div>
+          <input
+            id="input"
+            className={inputStyle}
+            type="text"
+            value={email}
+            placeholder="Your email address..."
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setIsError(false);
+            }}
+          />
+          {isError && (
+            <p className="error">Please provide a valid email address</p>
+          )}
+        </div>
         <button className="email-submit-btn" type="submit">
           Notify Me
         </button>
       </form>
-      {isError && <p className="error">Please provide a valid email address</p>}
+
       <img
         className="dashboard-img"
         src="./images/illustration-dashboard.png"
         alt="Ping logo"
       />
       <div className="social-media-links">
-        <i className="fa fa-facebook-official" aria-hidden="true"></i>
+        <i className="fa fa-facebook" aria-hidden="true"></i>
         <i className="fa fa-twitter" aria-hidden="true"></i>
         <i className="fa fa-instagram" aria-hidden="true"></i>
       </div>
